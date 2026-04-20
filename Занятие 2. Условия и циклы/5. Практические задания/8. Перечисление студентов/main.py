ee8d27683740e9f39dd298989901f3aeaf6a7ee9
unknown <vladimirovicp70@gmail.com>
2026-04-20 22:47:47 +0400

    feat: add 2.5 final

--- Занятие 2. Условия и циклы/5. Практические задания/8. Перечисление студентов/main.py
+++ Занятие 2. Условия и циклы/5. Практические задания/8. Перечисление студентов/main.py
@@ -1,3 +1,6 @@
 list_students = ["Маша", "Петя", "Саша", "Кирилл", "Оля"]
 
 # TODO распечатать в столбик все имена
+
+for students in list_students:
+    print(students)
--- Занятие 2. Условия и циклы/5. Практические задания/9. Оценки студентов/main.py
+++ Занятие 2. Условия и циклы/5. Практические задания/9. Оценки студентов/main.py
@@ -8,3 +8,6 @@ students = [
 ]
 
 # TODO Распечатать имена студентов с оценками выше тройки
+for student in students:
+    if student["grade"] > 3:
+        print(f"{student["name"]}. Оценка: {student["grade"]}")
