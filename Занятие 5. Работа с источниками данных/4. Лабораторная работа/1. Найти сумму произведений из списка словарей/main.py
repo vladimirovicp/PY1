eccbe943b9d8bb09a02a3a79866e9090f0559522
unknown <vladimirovicp70@gmail.com>
2026-04-26 00:38:48 +0400

    feat: add 5.4

--- Занятие 5. Работа с источниками данных/4. Лабораторная работа/1. Найти сумму произведений из списка словарей/main.py
+++ Занятие 5. Работа с источниками данных/4. Лабораторная работа/1. Найти сумму произведений из списка словарей/main.py
@@ -1,6 +1,14 @@
+import json
+FILENAME = "input.json"
+
 # TODO решите задачу
 def task() -> float:
-    ...
+    with open(FILENAME, encoding="utf-8") as f:
+        data = json.load(f)
+
+    res = sum( item["score"] * item["weight"] for item in data)
+
+    return round(res,3)
 
 
 print(task())
--- Занятие 5. Работа с источниками данных/4. Лабораторная работа/2. Конвертер из CSV в JSON формат/main.py
+++ Занятие 5. Работа с источниками данных/4. Лабораторная работа/2. Конвертер из CSV в JSON формат/main.py
@@ -1,3 +1,5 @@
+import csv
+import json
 # TODO импортировать необходимые модули
 
 
@@ -6,9 +8,15 @@ OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    ...  # TODO считать содержимое csv файла
+    # TODO считать содержимое csv файла
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+    with open(INPUT_FILENAME, 'r', encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",", lineterminator="\n")
+        data = list(reader)
+
+    # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, 'w', encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
